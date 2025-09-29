169275dcca42f24bf3d029405b219fb268aa4c2c
hamza <[hamzasaleem15793@gmail.com\]>
2025-09-29 21:39:53 +0500

    i write sum product wla program

--- hello.cpp
+++ hello.cpp
@@ -8,5 +8,28 @@ for (n=1 ; n<=4 ; n++){
 }
 cout<<n<<endl;
 return 0;
+#include <iostream>
+using namespace std;
 
+int main() {
+    int count = 0;
+    int num = 2;
+
+    while (count < 10) {
+        bool isPrime = true;
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            cout << num << " ";
+            count++;
+        }
+        num++;
+    }
+    cout << endl;
+    return 0;
+}
 }
